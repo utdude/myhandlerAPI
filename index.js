f341983d0fc22c68f795418606cfbe3dcecef606
utkarshrai369 <=>
2024-07-24 10:36:20 +0530

    mysql connection

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const app = express()
 
-app.get("/Login", (req,res) =>{
-    
+const mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "mysql.railway.internal",
+    user: "root",
+    port:3306,
+    password: "yjiFEBioMnuwbKWBWtLhfvAQtovfPmaO",
+    database: "railway"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+app.post("/Login", (req,res) =>{
+
+    res.send("helloworld");
+
 })
 
 app.listen(8000)
